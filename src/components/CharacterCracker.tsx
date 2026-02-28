b286b7e6a5f4d93bb54371335edf09c445f871c9
ThembinkosiMadiba <jedaiimadiba06@gmail.com>
2026-02-28 18:18:43 +0200

    Define CharacterCrackerProps interface
    Added TypeScript interface for CharacterCracker props.

--- src/components/CharacterCracker.tsx
+++ src/components/CharacterCracker.tsx
@@ -1,7 +1,13 @@
 import { motion } from 'motion/react';
 import { Lock, Unlock } from 'lucide-react';
 
-
+interface CharacterCrackerProps {
+  password: string;
+  currentPosition: number;
+  currentChar: string;
+  crackedChars: string[];
+  isActive: boolean;
+}
 
 const CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
 
